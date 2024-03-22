0c76f5572558a40b450b5beb3c3be90819522648
MicroYui <1048354041@qq.com>
2024-03-22 14:49:10 +0800

    feat: 请求到达矩阵实现

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import numpy as np
 
+# 应用请求次数
+app_1_request = 30
+app_2_request = 10
 # 行数rows为微服务数
 rows = 5
 # 列数cols为节点数
 cols = 5
 # 不可达时间
 max_time = 99999
+# 限流向量
+delta = np.random.rand(2)
+# 外部流量向量
+lambda_out = [app_1_request, app_2_request]
+# 实际进入流量
+lambda_in = delta * lambda_out
+# 应用对应的微服务
+start_service = [0, 3]
+# 接入点所在的节点
+access_node = [0, 3]
 
 # 实例部署情况，行表示微服务，列表示节点
 instance = np.random.randint(2, size=(rows, cols))
@@ -45,6 +58,52 @@ compute_time = np.array([
     [9, 9 / 6 * 5, 9 / 3 * 2, 9 / 2, 9 / 3],
     [78, 78 / 6 * 5, 78 / 3 * 2, 78 / 2, 78 / 3]
 ])
+
+# 请求到达率矩阵，即每个微服务在每个节点上的服务到达率
+# 行表示微服务，列表示节点
+request_arrive = np.zeros((rows, cols))
+
+
+# 根据当前转发函数、流量调控因子和服务依赖情况更新请求达到矩阵
+def update_request_arrive_array_matrix():
+    # 将矩阵清空
+    arrive = np.zeros((rows, cols))
+
+    # 先处理开始的微服务(无上游微服务)
+    for index in range(len(start_service)):
+        # 初始化每个app对应的矩阵
+        arrive_app = np.zeros((rows, cols))
+        # 获取接入点所在的节点
+        access = access_node[index]
+        # 获取app对应的第一个微服务
+        first_service = start_service[index]
+        # 获取当前节点与目标微服务所在节点的传输时间与处理能力
+        compute_and_transmit_time = get_compute_and_transmit_time(access, first_service)
+        # 获取转发概率向量
+        route_vector = route(compute_and_transmit_time[0], compute_and_transmit_time[1])
+        request_dispatch = np.array(route_vector) * lambda_out[index]
+        # 将请求到达率写入矩阵
+        arrive_app[first_service, :] += request_dispatch
+
+        # 处理此app链路的所有微服务带来的请求
+        current_service = first_service
+        # 如果有服务依赖就继续往下找
+        while np.array_equal(service_dependency[current_service, :], np.zeros(rows)) is False:
+            service_vector = service_dependency[current_service, :]
+            for service_index in range(len(service_vector)):
+                # 找到下游微服务
+                if service_vector[service_index] == 1:
+                    next_service = service_index
+                    current_service_node_vector = get_nodes_of_a_service(current_service)
+                    # 遍历当前服务所有节点
+                    for current_service_node in current_service_node_vector:
+                        compute_and_transmit_time = get_compute_and_transmit_time(current_service_node, next_service)
+                        route_vector = route(compute_and_transmit_time[0], compute_and_transmit_time[1])
+                        request_dispatch = np.array(route_vector) * arrive_app[current_service, current_service_node]
+                        arrive_app[next_service, :] += request_dispatch
+                    current_service = next_service
+        arrive += arrive_app
+    return arrive
 
 
 # 获取当前节点到所有目标微服务所在节点的传输时间(假设不可到达的传输时间为99999ms)
@@ -88,7 +147,7 @@ def route(compute_vector: list, transmit_vector: list):
 
 
 # 获取一个服务部署的所有节点的列表
-def get_nodes_of_a_service(service: int):
+def get_nodes_of_a_service(service: int) -> list:
     node_vector = []
     for node in range(len(instance[service])):
         if instance[service, node] == 1:
@@ -96,33 +155,11 @@ def get_nodes_of_a_service(service: int):
     return node_vector
 
 
-# 当前节点当前服务被请求的总数
-def get_request_number_by_node_and_service(node, service) -> int:
-    # 找到所有上游微服务
-    services = []
-    count = 0
-    for row in service_dependency:
-        if row[service] == 1:
-            services.append(count)
-        count += 1
-
-    request_number = 0
-
-    # 遍历所有上游微服务，获取所有的请求数
-    for upstream_service in services:
-        upstream_nodes = get_nodes_of_a_service(upstream_service)
-        # 遍历本服务部署的所有节点
-        for upstream_node in upstream_nodes:
-            # 获取此服务此节点的服务速率
-            compute_ability = 1 / compute_time[upstream_service, upstream_node]
-            # 根据路由函数得到向目标节点转发的速率
-            compute_and_transmit_time = get_compute_and_transmit_time(upstream_node, service)
-            route_vector = route(compute_and_transmit_time[0], compute_and_transmit_time[1])
-            request_number += compute_ability * route_vector[service]
-
-    return request_number
-
-
 if __name__ == '__main__':
     print(instance)
-    print(get_nodes_of_a_service(2))
+    # print(get_nodes_of_a_service(2))
+    print(delta)
+    print(lambda_in)
+    print(request_arrive)
+    request_arrive = update_request_arrive_array_matrix()
+    print(request_arrive)
